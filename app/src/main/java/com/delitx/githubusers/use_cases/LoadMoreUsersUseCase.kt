77895a55d292933718a1abaed54fe6a803009b46
DeLitX <dimkanan@gmail.com>
2022-02-03 13:33:00 +0200

    added LoadMoreUsersUseCase.kt

---
+++ app/src/main/java/com/delitx/githubusers/use_cases/LoadMoreUsersUseCase.kt
@@ -0,0 +1,11 @@
+package com.delitx.githubusers.use_cases
+
+import com.delitx.githubusers.common.DataState
+import com.delitx.githubusers.domain.repository.UsersRepository
+
+class LoadMoreUsersUseCase(
+    private val _repository: UsersRepository
+) {
+    val users = _repository.users
+    suspend fun invoke(): DataState<Unit> = _repository.loadMoreUsers()
+}
